085b2fd0307c84df469fa114a8f31655836fc5b9
wilman orellano <wilmanjunior2001@gmail.com>
2020-06-30 18:13:59 -0500

    se añadio file de la base de datos

---
+++ database/prueba.sql
@@ -0,0 +1,134 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 01-07-2020 a las 00:59:56
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `prueba`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `menu`
+--
+
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL,
+  `plato` varchar(45) DEFAULT NULL,
+  `precio` varchar(40) DEFAULT NULL,
+  `descripcion` text DEFAULT NULL,
+  `fecha_registro` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `menu`
+--
+
+INSERT INTO `menu` (`id`, `plato`, `precio`, `descripcion`, `fecha_registro`) VALUES
+(43, 'pescado', '10.000', 'pescado', '2020-06-30');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sessions`
+--
+
+CREATE TABLE `sessions` (
+  `session_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `expires` int(11) UNSIGNED NOT NULL,
+  `data` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `sessions`
+--
+
+INSERT INTO `sessions` (`session_id`, `expires`, `data`) VALUES
+('ONeCcFsPbV5QL5DzQ5HH9jgnqQEaIS2n', 1593577445, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"flash\":{},\"passport\":{\"user\":1}}'),
+('lztf9L9W35ncoIduUgKMn6ZL52zsU57j', 1593644283, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"flash\":{\"error\":[\"Missing credentials\"]},\"passport\":{\"user\":1}}'),
+('vnf5-BOo2-3udC5fdJslPqm2pPy2N4DK', 1593569962, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"flash\":{},\"passport\":{\"user\":1}}');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `fullname` varchar(20) DEFAULT NULL,
+  `username` varchar(30) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `password` varchar(60) NOT NULL,
+  `mobile` double DEFAULT NULL,
+  `cedula` double DEFAULT NULL,
+  `sexo` varchar(20) DEFAULT NULL,
+  `tyc` varchar(10) DEFAULT NULL,
+  `fecha_inicio` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `fullname`, `username`, `email`, `password`, `mobile`, `cedula`, `sexo`, `tyc`, `fecha_inicio`) VALUES
+(1, 'wilman', 'orellano', 'admin', 'admin@gmail.com', '$2a$10$6QKTEh7ppQaQeZtV/oFykemYYz2Wb.BiSwqWC2b3G8vwu.0uQO1UW', 1111111111, 1234567890, 'Hombre', 'Acepto', '2020-06-29');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`session_id`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/routes/links.js
+++ src/routes/links.js
@@ -3,11 +3,11 @@ const router  = express.Router();
 const pool = require('../database');
 const {isLoggedIn } = require('../lib/auth');
 
-router.get('/new', isLoggedIn,(req, res)=>{
+router.get('/profile/new', isLoggedIn,(req, res)=>{
   res.render('links/new');
 });
 
-router.post('/new', isLoggedIn, async(req, res) => {
+router.post('/profile/new', isLoggedIn, async(req, res) => {
   const {plato, precio, descripcion} = req.body;
   const newPlato = {
     plato,
@@ -16,28 +16,28 @@ router.post('/new', isLoggedIn, async(req, res) => {
   };
   await pool.query('INSERT INTO menu set ?', [newPlato]);
   req.flash('success', ' Plato creado con exito.');
-  res.redirect('/list');
+  res.redirect('/profile/list');
 });
 
-router.get('/list', isLoggedIn, async(req, res) =>{
+router.get('/profile/list', isLoggedIn, async(req, res) =>{
   const lista = await pool.query('SELECT * FROM menu');
   res.render('links/list', {lista});
 });
 
-router.get('/delete/:id', isLoggedIn, async(req, res) =>{
+router.get('/profile/delete/:id', isLoggedIn, async(req, res) =>{
   const { id } = req.params;
   await pool.query('DELETE FROM menu WHERE Id = ?', [id]);
   req.flash('danger', '  Plato eliminado.');
-  res.redirect('/list');
+  res.redirect('/profile/list');
 });
 
-router.get('/edit/:id', isLoggedIn, async(req, res) =>{
+router.get('/profile/edit/:id', isLoggedIn, async(req, res) =>{
   const { id } = req.params;
   const plato = await pool.query('SELECT * FROM menu WHERE Id = ?', [id]);
   res.render('links/edit', { plato: plato[0]});
 });
 
-router.post('/edit/:id', isLoggedIn, async(req, res) =>{
+router.post('/profile/edit/:id', isLoggedIn, async(req, res) =>{
   const { id } = req.params;
   const { plato, precio, descripcion} = req.body;
   const newPlato = {
@@ -47,7 +47,7 @@ router.post('/edit/:id', isLoggedIn, async(req, res) =>{
   };
   await pool.query('UPDATE menu set ? WHERE Id = ?', [newPlato, id]);
   req.flash('success', '  Plato Acualizado.');
-  res.redirect('/list');
+  res.redirect('/profile/list');
 });
 
 
