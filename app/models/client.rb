618f03ff1df11b7ab01c7504ebb3ad883db7f6e2
etbgm03 <esfergo150618@gmail.com>
2020-09-24 09:53:39 -0500

    mejora de cerrar sesion

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,5 @@
 class Client < ApplicationRecord
-  has_many :sales
+  has_many :sales, dependent: :destroy
   validates :nombre, format: { with: /\A[a-z A-Z]+\z/, message: "debe incluir sólo letras" }
   validates_format_of :correo,{with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: 'debe ser válido!'}
   validates :direccion, presence: true
