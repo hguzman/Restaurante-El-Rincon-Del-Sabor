69e340fed925e1540efc41b62dd50c30f13caebf
wilman orellano <wilmanjunior2001@gmail.com>
2020-10-15 16:35:43 -0500

    se añadieron campos a la tabla user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,54 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :destroy, :update]
+
+  def index
+      # authorize User
+      @users=User.all
+    end
+
+    def show
+    end
+
+    def new
+      @user= User.new
+       # authorize @user
+    end
+
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        redirect_to users_path
+        flash.notice= "User creado"
+
+      else
+        render :new
+      end
+
+    end
+
+    def update
+      if @user.update(user_params)
+        redirect_to users_path
+        flash.notice="User editado"
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @user.destroy
+      redirect_to users_path
+      flash.alert="User eliminado"
+    end
+
+    def edit
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+  def user_params
+      params.require(:user).permit(:nombres,:email,:password,:password_confirmation,:apellidos,:direccion,:telefono,:sexo, :cedula)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_05_225616) do
+ActiveRecord::Schema.define(version: 2020_10_15_204059) do
 
   create_table "categories", force: :cascade do |t|
     t.string "nombre"
@@ -84,6 +84,12 @@ ActiveRecord::Schema.define(version: 2020_10_05_225616) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nombres"
+    t.string "apellidos"
+    t.string "cedula"
+    t.string "direccion"
+    t.string "telefono"
+    t.string "sexo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
