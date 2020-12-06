74f1a754178cbd47dfa09487c3551a8a490929de
etbgm03 <esfergo150618@gmail.com>
2020-12-06 12:16:07 -0500

    se arregló el valor ingresado de la existencia. Se eliminó fk de la tabla clientes.

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,11 +3,11 @@ class Client < ApplicationRecord
   validates :nombre, format: { with: /\A[a-z A-Z]+\z/, message: "debe incluir sólo letras" }
   validates_format_of :correo,{with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: 'debe ser válido!'}
   validates :direccion, presence: true
-  validates :cedula, length: { in: 10..10 },numericality: true
-  validates :telefono, length: { in: 10..10 },numericality: true
+  validates :cedula, length: { in: 7..10 },numericality: true
+  validates :telefono, length: { in: 7..10 },numericality: true
 
   def to_s
     self.nombre
   end
-  belongs_to :user, dependent: :destroy
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_215306) do
+ActiveRecord::Schema.define(version: 2020_12_06_171406) do
 
   create_table "categories", force: :cascade do |t|
     t.string "nombre"
@@ -28,8 +28,6 @@ ActiveRecord::Schema.define(version: 2020_11_16_215306) do
     t.boolean "estado"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.index ["user_id"], name: "index_clients_on_user_id"
   end
 
   create_table "delayed_jobs", force: :cascade do |t|
