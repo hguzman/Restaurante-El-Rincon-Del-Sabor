d13a5615b560e656c133d91550d9b94fa050c575
Andres Rodriguez <andresfr0428@gmail.com>
2020-12-07 08:25:36 -0500

    Terminado

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,13 +1,13 @@
-class ProfilesController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_user, :only [:update]
-
-  def update
-  end
-
-  private
-
-  def set_user
-    @user = User.find(params[:id])
-  end
-end
+# class ProfilesController < ApplicationController
+#   before_action :authenticate_user!
+#   before_action :set_user, :only [:update]
+#
+#   def update
+#   end
+#
+#   private
+#
+#   def set_user
+#     @user = User.find(params[:id])
+#   end
+# end
